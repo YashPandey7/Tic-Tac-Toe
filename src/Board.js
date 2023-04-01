4ff8e3c28e5b858fc40a722560e9ba3521bdde26
Yash Pandey <yashdevesh10@gmail.com>
2023-04-01 22:15:50 +0530

    Adding Files

--- src/Board.js
+++ src/Board.js
@@ -35,8 +35,84 @@ const Board = ({ reset, setReset, winner, setWinner }) => {
         }
     }
 
+    // UseEffect hook used to reset the board whenever 
+    // a winner is decided
+    useEffect(() => {
+        // Clearing the data state
+        setData(['', '', '', '', '', '', '', '', '']);
+
+        // Getting all the children(cells) of the board
+        const cells = boardRef.current.children;
+
+        // Clearing out the board
+        for(let i=0; i<9; i++){
+            cells[i].innerText = '';
+        }
+
+        // Resetting the turn to player 0
+        setTurn(0);
+
+        // Resetting the winner
+        setWinner('');
+        setReset(false);
+    }, [reset, setReset, setWinner]);
 
 
+    // useEffect hook used to check for a winner
+    useEffect(() => {
+
+        // Checks for the win condition in rows
+        const checkRow = () => {
+            let ans = false;
+            for(let i=0; i<9; i += 3){
+                ans |= (data[i] === data[i+1] && data[i] === data[i+2] && data[i] !== '');
+            }
+            return ans;
+        }
+
+        // Checks for the win condition in cols
+        const checkCol = () => {
+            let ans = false;
+            for(let i=0; i<9; i += 3){
+                ans |= (data[i]=== data[i+3] && data[i] === data[i+6] && data[i] !== '');
+            }
+            return ans;
+        }
+
+        // Checks for the win condition in diagonals
+        const checkDiagonal = () => {
+            return ((data[0] === data[4] && data[0] === data[8] && data[0] !== '') || (data[2] === data[4] && data[2] === data[6] && data[2] !== ''));
+        }
+
+        // Checks if at all a win condition is present
+        const checkWin = () => {
+            return (checkRow() || checkCol() || checkDiagonal());
+        }
+
+        // Checks for a tie
+        const checkTie = () => {
+            let count = 0;
+            data.forEach((cell) => {
+                if(cell !== ''){
+                    count++;
+                }
+            })
+            return count === 9 ;
+        }
+
+        // Setting the winner in case of a win
+        if(checkWin())
+        {
+            setWinner(turn === 0 ? "Player 2 Wins!" : "Player 1 Wins!");
+        }
+        else if(checkTie())
+        {
+            setWinner("It's a Tie!");
+        }
+
+    })
+
+    
     return (
         <div ref={boardRef} className='board'>
             <div className='input input-1' onClick={(e) => draw(e,1)}></div>
